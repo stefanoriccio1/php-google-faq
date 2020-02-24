5541f7919fd6ab73eae69d0fb970c50a0c8c7d5a
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-24 15:14:48 +0100

    inizio impostazione php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  $database = [
+     'aticle1' =>[
+       'title' => 'Titolo1',
+       'text' => 'Testo1'
+     ],
+     'aticle2' =>[
+       'title' => 'Titolo2',
+       'text' => 'Testo2'
+     ],
+     'aticle3' =>[
+       'title' => 'Titolo3',
+       'text' => 'Testo3'
+     ],
+     'aticle4' =>[
+       'title' => 'Titolo4',
+       'text' => 'Testo4'
+     ],
+  ];
+var_dump($database);
+?>
