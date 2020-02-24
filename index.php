1bd58840ac800e6ea591bcaaba957fccaeddfc5b
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-24 15:41:41 +0100

    proseguo nel ragionamento e intanto stampo

--- index.php
+++ index.php
@@ -20,10 +20,40 @@
 var_dump($database);
 
 foreach ($database as $articles => $data) {
-  // var_dump($articles .' - ' .$data);
+  var_dump($articles);
 
   foreach ($data as $key => $value) {
-    var_dump($value);
+    // var_dump($value);
   }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Google FAQ</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+
+      </div>
+    </header>
+    <main>
+      <div class="wrapper">
+        <ul class="faq">
+          <?php foreach ($database as $articles => $data) {?>
+            <li class= "article"> <?php echo $articles ?>
+              <ul>
+
+              </ul>
+            </li>
+          <?php } ?>
+        </ul>
+      </div>
+
+    </main>
+  </body>
+</html>
